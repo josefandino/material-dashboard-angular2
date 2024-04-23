5c8260920ecc8117f035cb1f5ecdfd1434f21c02
josefandino <josefandinod@gmail.com>
2024-04-23 17:25:28 -0500

    Fix import statement in admin-layout.module.ts Comment out upgrade route in sidebar.component.ts Fix formatting in app.routing.ts Remove unnecessary code in index.html

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -15,6 +15,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatRippleModule} from '@angular/material/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
+// import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatSelectModule} from '@angular/material/select';
 
